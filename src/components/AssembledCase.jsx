7dda46b5ec4b89e0263eb4a16764f3315da2357a
umang <umangpawar2005@gmail.com>
2026-04-06 19:30:35 +0530

    removed react three fiber

--- src/components/AssembledCase.jsx
+++
@@ -1,7 +0,0 @@
-import { useGLTF } from '@react-three/drei';
-
-export default function AssembledCase() {
-  const { scene } = useGLTF('/assembled_case.glb');
-
-  return <primitive object={scene} scale={0.1} />;
-}
